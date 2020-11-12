b298ad2125a0faa3a7211b93d88e04f6c2452ecd
Daniel <bukaro.dany@gmail.com>
2020-11-11 22:28:12 -0500

    Get a task or list of tasks and adding the route

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import psycopg2
@@ -41,6 +41,21 @@ def create_task():
     db.session.commit()
 
     return task_schema.jsonify(new_task,)
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    all_tasks = Task.query.all()
+    result = tasks_schema.dump(all_tasks)
+
+    return jsonify(result)
+
+
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+    task = Task.query.get(id)
+
+    return task_schema.jsonify(task)
 
 
 if __name__== '__main__':
