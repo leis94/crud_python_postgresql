c1fb5e3523b1985ecee6589e49f39cff75320f1e
Daniel <bukaro.dany@gmail.com>
2020-11-11 22:18:40 -0500

    Create Task and adding the route

--- src/app.py
+++ src/app.py
@@ -10,11 +10,37 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(70), unique=True)
+    description = db.Column(db.String(100))
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+db.create_all()
+
+
+class TaskSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description')
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
+
+
 @app.route('/tasks', methods=['POST'])
 def create_task():
-    print(request.json)
-    return 'received'
+    title = request.json['title']
+    description = request.json['description']
 
+    new_task = Task(title, description)
+    db.session.add(new_task)
+    db.session.commit()
+
+    return task_schema.jsonify(new_task,)
 
 
 if __name__== '__main__':
